0b5c09e31dff0608d373b25322db2b2b878cfd74
Mary <maryisnotgrumpy@gmail.com>
2020-01-28 18:56:26 -0500

    1/28/2020

---
+++ classwork/1282020.js
@@ -0,0 +1,70 @@
+let myName= 'Mary Elizabeth';
+let myMovie = 'Charade';
+let myNumber = 63;
+let myFood = 'pizza';
+let myColor = 'green';
+
+let myFavoriteThings =
+    [
+        myName,
+        myColor,
+        myFood,
+        myMovie,
+        myNumber
+    ];
+
+//console.log ("myName:",  myName);
+//console.log ("myFavoriteThings:",  myFavoriteThings);
+
+let myPhrase = 'I am Mary and my favorite movie is ' + myMovie + '.';
+
+//console.log (myPhrase);
+
+//buttstuff
+
+let subTotal = 40.00;
+console.log('Subtotal $', subTotal);
+
+if ( subTotal >=50) {
+
+    let currentDate = new Date();
+    if currentDate. getDay() === 2) ;}
+
+    console.log ("you get some stuff off");}
+
+
+
+
+
+        console.log("you got a discount fuck yeah!")
+    }
+
+}
+else if (subTotal >=40.00) {
+
+    console.log("tiny discount bitch")
+
+}
+
+// Loops
+
+    for (let C = 0
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+else {
+    console.log("bummer bitch")
+};
