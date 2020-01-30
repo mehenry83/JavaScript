2b18d37b5e9e36aeabb356cce2863bf9500c1fa5
Mary <maryisnotgrumpy@gmail.com>
2020-01-30 17:51:31 -0500

    completed homework 1 for real.

---
+++ homework/homework_1.js
@@ -0,0 +1,44 @@
+//Mary Henry
+//Homework 1
+//1-20-20
+//JavaScript-172
+
+let myName = "Mary";
+let myMajor = "going here forever";
+let funFact = "There is a chicken in my basement";
+console.log("My name is",myName,"My Major is" ,myMajor,"and a fun fact about me is",funFact);
+
+
+var myEmail ="maryisnotgrumpy@gmail.com" ;
+console.log(myEmail);
+
+const className = "CIS-172";
+console.log("I am using JS in", className);
+
+for (let c = 1; c <14; c++)
+{
+    console.log(c + " doing a for loop");
+}
+
+let c = 13;
+while (true)
+{
+    console.log(c + " doing a while loop");
+    c -= 1;
+    if (c == 0)
+    {
+        break;
+    }
+}
+
+if( myName === "Mr. Banks" )
+{
+    console.log("Welcome back Professor!!");
+}
+else
+{
+    console.log("Welcome Back",myName,"How's your Major of" ,myMajor,"going for you?");
+}
+
+
+
