4b87e43a33e7f037e0d8eb8e10d02717c7a85421
Mary <maryisnotgrumpy@gmail.com>
2020-02-13 16:34:10 -0500

    homework 2 v2

---
+++ homework/homework_2/calculator.js
@@ -0,0 +1,25 @@
+/**
+ *
+ *
+ * @category    Homework 2
+ *              javascript calculator
+ *              now with OBJECTS!
+ * @author      Mary Henry <maryisnotgrumpy@gmail.com>
+ * @date        2020.02.12
+ **/
+
+
+
+
+function takeValue(x) {
+    document.getElementById('inputwindow').value += x;
+}
+
+function clearInput(y) {
+    document.getElementById('inputwindow').value = y;
+}
+
+function calculateResult() {
+    var result = eval(document.getElementById('inputwindow').value);
+    document.getElementById('inputwindow').value = result;
+}
