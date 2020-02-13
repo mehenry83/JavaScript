35c6af3013690a7ef7c2c6ad6dd4f2caa9adb933
Mary <maryisnotgrumpy@gmail.com>
2020-02-13 18:20:00 -0500

    quiz 2

---
+++ quiz/quiz2.js
@@ -0,0 +1,46 @@
+/**
+ * Quiz 2 focuses on functions and events
+ *
+ * @category    Quiz 2
+ * @author      Chad Banks <crbanks1@hfcc.edu>
+ * @author      Mary Henry <maryisnotgrumpy@gmail.com>
+ * @date        2020.02.13
+ * @link
+    */
+
+// 3 pts
+// 1. Define a function that take two arguments, you can choose the name.
+//      The function should them multiply them together and return the result
+
+function MultiplyNumbers (number1, number2) {
+    return number1 * number2;
+}
+console.log(MultiplyNumbers(4, 3));
+
+
+
+// 2 pts
+// 2. Create a button with an id of multiply, with text that says "Multiple 4 and 7"
+//      Add an onclick event to this button that calls the function above, passing in 4 and 7.
+<input type="button" id="multiply" value="4, 7" onclick="MultiplyNumbers(document.getElementById('multiple 4 x7').value)">
+
+// 2 pts
+// 3. Write a function that displays a welcome message to the user in the dev console or anywhere on the page.
+
+    function welcomeMessage()
+    {
+        console.log("Welcome to The Great and Secret Show");
+    }
+// 3 pts
+// 4. Create a button with an id of welcome, with text that says "Display Welcome"
+//      Add a JS created listener click event to this button that calls the welcome function above.
+
+    <input type="button" id="Display Welcome" value="welcome" onclick="addEventListener(welcomeMessage().value)">
+// 1 ex pt
+// 5. Create an on hover event for one of the buttons above.
+//      This event can fire any function you would like.
+    function makeBig(x) {
+        x.style.height = "300px";
+        x.style.width = "300px";
+    }
+  /**  <input type="button" id="multiply" value="multiple 4x7"*/ onmouseover="makeBig(this)" /**onclick="MultiplyNumbers(document.getElementById('4, 7').value)">
