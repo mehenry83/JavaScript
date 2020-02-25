9d0d1dba15fcd26f7ac459b304f83617f6bc1cb3
Mary <maryisnotgrumpy@gmail.com>
2020-02-25 15:39:02 -0500

    I forgot to commit homework 3. :/

---
+++ homework/homework3/twitter.js
@@ -0,0 +1,33 @@
+var tweet = [];
+
+
+function tweetBuilder()
+{
+
+
+    let id = document.getElementById("id");
+    let tweetMessage = document.getElementById("tweetMessage");
+    let tweetLikes = document.getElementById("tweetLikes");
+    let tweetShares = document.getElementById("tweetShares");
+    let tweetComments = document.getElementById("tweetComments");
+    let tweetUser = document.getElementById("tweetUser");
+    let tweetTime = document.getElementById("tweetTime");
+
+    let tweetCollection =
+        {
+            id:       id.value,
+            message:       tweetMessage.value,
+            likes:      tweetLikes.value,
+            shares:     tweetShares.value,
+            comments:    tweetComments.value,
+            username:  tweetUser.value,
+            time:  tweetTime.value,
+        };
+
+    tweet.push(tweetCollection);
+
+    console.log("Tweet:", tweetCollection);
+}
+
+let TweetBtn = document.getElementById("Tweet");
+TweetBtn.addEventListener( "click", tweetBuilder );
