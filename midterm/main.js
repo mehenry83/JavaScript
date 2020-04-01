8737bca64b8ad702f87173a9c80194c451ab1e0c
Chad Banks <chadrbanks@yahoo.com>
2020-04-01 01:41:20 -0400

    Midterm grade.

--- midterm/main.js
+++ midterm/main.js
@@ -6,7 +6,7 @@
  * @author      Chad Banks <crbanks1@hfcc.edu>
  * @author      Mary Henry <maryisnotgrumpy@gmail.com>
  * @date        3.5.2020
- * @grade
+ * @grade       85 / 100
  **/
 
 let Blueit = {
@@ -47,12 +47,16 @@ let Blueit = {
     {
         Blueit.sendVoteToApi( e.target.id, -1 );
     },
-    try {
     sendVoteToApi: function( buttonId, Value )
     {
-        // Value can be 1 or negative one
-        $.ajax( Blueit.siteApiUrl, {action:"vote", pid:buttonId, val:Value} );
-    }} catch {error};,
+        try{
+            // Value can be 1 or negative one
+            $.ajax( Blueit.siteApiUrl, {action:"vote", pid:buttonId, val:Value} );
+        } catch (error)
+        {
+            console.error(err);
+        }
+    },
     sendNewPostToApi: function( dataObj )
     {
         $.ajax( Blueit.siteApiUrl, {action:"post", pid:buttonId, val:Value, data:dataObj} );
