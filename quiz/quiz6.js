39ec7524abf4cb137eba921eb2def38c4fe1d0a8
Chad Banks <chadrbanks@yahoo.com>
2020-04-21 01:59:15 -0400

    Quiz 7

--- quiz/quiz6.js
+++ quiz/quiz6.js
@@ -5,7 +5,7 @@
  * @author      Chad Banks <crbanks1@hfcc.edu>
  * @author      Mary Henry <maryisnotgrumpy@gmail.com>
  * @date        2020.04.16
- * @grade
+ * @grade       9 / 10
  */
 
 // 1 pt
@@ -16,19 +16,19 @@
 // 2. Assume this is an HTML file, add a tag to import a local jQuery file called jquery.min.js
     <script src = "jquery.min.js"></script>
 
-// 2 pts
+// 1.5 / 2 pts
 // 3. Use jQuery to select the following div below and store it in a variable called div.
 // <div id="newUserSignUpModal" style="display:none;" ><p>There is form data in this div.</p></div>
-var divs = $( "#newUserSignUpmodal" ).has( "p" );
+var divs = $( "#newUserSignUpmodal" )
 
 // 1 pt
 // 4. The div selected above is currently hidden, use a jQuery method on this div variable to make it visible.
 $("#newUserSignUpmodal").show();
 
-// 2 pts
+// 1.5 / 2 pts
 // 5. Assume the user is done using the modal in the div above.
 //      Use a jQuery method to slowly fade the div out over a time span of 4 seconds.
-$("#newUserSignUpmodal").fadeIn('slow').delay(4000).fadeOut('slow');
+$("#newUserSignUpmodal").fadeOut(4000);
 
 // 3 pts
 // 6. List three more methods below that are available via the jQuery API.
