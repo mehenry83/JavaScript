170752fb914dad9130774cbcc7134782966a4f5a
Mary <maryisnotgrumpy@gmail.com>
2020-05-07 12:04:17 -0400

    Homework 5

---
+++ homework/homework5/index.js
@@ -0,0 +1,41 @@
+/**
+ *
+ *
+ * @category    homework 5
+ * @author      Mary Henry <maryisnotgrumpy@gmail.com>
+ * @date        2020.04.20
+ * @grade           /10
+ */
+
+
+
+ let errorHandler = {
+
+    "init": function () {
+        let errorBtn = document.getElementById('err');
+        errorBtn.addEventListener('click', errorHandler.checkDate)
+    },
+    checkDate: function () {
+
+        let today = new Date();
+        let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        let checkDate = date + ' ' + time;
+        console.log(checkDate);
+
+        try {
+            try {
+                if (!(0 === checkDate % 2))
+                console.log("Welcome to  class.");
+            } catch (err) {
+                console.error("Enjoy your day!", err);
+                throw err;
+            }
+        } catch (e) {
+            console.error("!");
+        }
+    }
+};
+
+
+        window.onload = errorHandler.init;
